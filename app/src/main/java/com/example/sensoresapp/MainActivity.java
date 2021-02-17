1426232d209bb9dade37132d5879f94b5f884df8
CarlosMartinz <usis044220@ugb.edu.sv>
2021-02-16 20:02:48 -0600

    appMedia

--- app/src/main/java/com/example/sensoresapp/MainActivity.java
+++ app/src/main/java/com/example/sensoresapp/MainActivity.java
@@ -14,24 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnCamara = (Button)findViewById(R.id.btnCamara);
 
-        btnCamara.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent next = new Intent(MainActivity.this, CamaraActivity.class);
-                startActivity(next);
-                finish();
-            }
-        });
-
-        Button btnProximidad = (Button)findViewById(R.id.btnProximidad);
-        btnProximidad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent next = new Intent(MainActivity.this, ProximidadActivity.class);
-                startActivity(next);
-            }
-        });
     }
 }
